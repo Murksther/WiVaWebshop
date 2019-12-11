41d972b214ee910d8188fd113cd1a2932bb933f8
Murksther <Mark.v.Drie@gmail.com>
2019-12-11 23:50:46 +0100

    Added User Role, and Dependencies used in the future

---
+++ WiVaServer/src/main/java/com/example/WiVaServer/model/RoleName.java
@@ -0,0 +1,7 @@
+package com.example.WiVaServer.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
+
---
+++ WiVaServer/src/test/java/com/example/WiVaServer/WiVaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WiVaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiVaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
