f9841505c574ca8ac6bba70551f8804b392fdcea
Murksther <Mark.v.Drie@gmail.com>
2019-12-12 19:46:38 +0100

    Added extra payloads to check availability of usernames, email etc. Also added controller to handle this

---
+++ WiVaServer/src/main/java/com/example/WiVaServer/user/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.example.WiVaServer.user.controller;
+
+import com.example.WiVaServer.generalapi.exception.ResourceNotFoundException;
+import com.example.WiVaServer.user.model.User;
+import com.example.WiVaServer.user.payload.UserIdentityAvailability;
+import com.example.WiVaServer.user.payload.UserProfile;
+import com.example.WiVaServer.user.payload.UserSummary;
+import com.example.WiVaServer.user.repository.UserRepository;
+import com.example.WiVaServer.user.security.UserPrincipal;
+import com.example.WiVaServer.user.security.CurrentUser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+    @GetMapping("/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping("/users/{username}")
+    public UserProfile getUserProfile(@PathVariable(value = "username") String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", username));
+
+        UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt());
+
+        return userProfile;
+    }
+}
