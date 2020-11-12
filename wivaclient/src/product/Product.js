8ad47131fdc97dd739e90334f92e330f94be7853
Murksther <Mark.v.Drie@gmail.com>
2020-11-12 14:34:49 +0100

    Changed product styling

--- wivaclient/src/product/Product.js
+++ wivaclient/src/product/Product.js
@@ -1,38 +1,43 @@
 import React, { Component } from 'react';
 import './Product.css';
 import { Avatar } from 'antd';
-import { Link } from 'react-router-dom';
 import { getAvatarColor } from '../util/Colors';
+import { Row, Col } from 'antd';
 
 class Product extends Component {
 
     render() {
 
         return (
-            <div className="product-content">
-                <div className="product-header">
-                    <div className="product-creator-info">
-                        <Link className="creator-link" to={`/products/${this.props.product.name}`}>
-                            <Avatar className="product-creator-avatar"
-                                    style={{ backgroundColor: getAvatarColor(this.props.product.name)}} >
-                                {this.props.product.name.toUpperCase()}
-                            </Avatar>
-                        </Link>
+            <Row className="product-content">
+                <Col span ={8} className="product-header">
+                    <div className="product-image">
+                        <Avatar className="product-creator-avatar"
+                                style={{ backgroundColor: getAvatarColor(this.props.product.name)}} >
+                            {this.props.product.name.toUpperCase()}
+                        </Avatar>
                     </div>
-                </div>
-                <div className="product-question">
-                    {this.props.product.description}
+                </Col>
+                <Col span={16}>
                     <div className="product-question">
+                        {this.props.product.name}
                     </div>
-                    {this.props.product.usedMaterial}
-                </div>
-                <div className="product-question">
-                    {this.props.product.price}
-                </div>
-                <div className="product-question">
-                    {this.props.product.availableUnits}
-                </div>
-            </div>
+                    <div className="product-question">
+                        Omschrijving:
+                        {this.props.product.description.split('\n').map(str => <div>{str}</div>)}
+                    </div>
+                    <div className="product-question">
+                        Gebruikt materiaal:
+                        {this.props.product.usedMaterial.split('\n').map(str => <div>{str}</div>)}
+                    </div>
+                    <div className="product-question">
+                        Prijs: € {this.props.product.price}
+                    </div>
+                    <div className="product-question">
+                        Aantal beschikbaar: {this.props.product.availableUnits}
+                    </div>
+                </Col>
+            </Row>
         );
     }
 }
